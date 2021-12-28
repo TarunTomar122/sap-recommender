f2a9eebada785e645d91c34f374536a2f1d46fa6
Billie@122 <tomartarun2001@gmail.com>
2021-12-28 12:35:35 +0530

    initialized connection to db

--- api/app.py
+++ api/app.py
@@ -1,11 +1,11 @@
 from flask import Flask
+import db
+
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return "hello world"
-
+@app.route("/")
+def test():
+    return "Connected to the data base!"
 
 if __name__ == '__main__':
     app.run()
---
+++ api/db.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_pymongo import pymongo
+import certifi
+ca = certifi.where()
+
+
+CONNECTION_STRING = "mongodb+srv://admin:admin1234@sap.05vye.mongodb.net/sap?retryWrites=true&w=majority"
+client = pymongo.MongoClient(CONNECTION_STRING, tlsCAFile=ca)
+
+db = client.get_database('sap')
+user_collection = pymongo.collection.Collection(db, 'articles')
