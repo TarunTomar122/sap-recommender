60aa7a0e54e0811b6372dc2f05c42ccb8a59081f
Billie@122 <tomartarun2001@gmail.com>
2022-01-02 16:54:31 +0530

    added scheduling for get articles

--- api/app.py
+++ api/app.py
@@ -1,14 +1,29 @@
-from flask import Flask,jsonify,request
+from flask import Flask, jsonify, request
 from werkzeug.wrappers import response
 import db
 import pymongo
 from scraper import AtlanticScraper, MediumScraper
+from flask_apscheduler import APScheduler
+
+# set configuration values
+class Config:
+    SCHEDULER_API_ENABLED = True
 
 app = Flask(__name__)
+app.config.from_object(Config())
 
+# initialize scheduler
+scheduler = APScheduler()
+# if you don't wanna use a config, you can set options here:
+# scheduler.api_enabled = True
+scheduler.init_app(app)
+scheduler.start()
 
-@app.route("/")
-def home():
+# cron examples
+@scheduler.task('cron', id='add_articles', hour='*')
+def add_articles():
+
+    print("Adding articles")
 
     atlanticScraper = AtlanticScraper()
     mediumScraper = MediumScraper()
@@ -24,52 +39,56 @@ def home():
         if db.db.articles.find_one({"title": article['title']}) is None:
             db.db.articles.insert_one(article)
 
-    return "saved articles"
 
 @app.route("/get_articles")
 def display():
-    response={'data':[],'error':False}
+    response = {'data': [], 'error': False}
     try:
-        articles=db.db.articles.find().sort([('date',pymongo.DESCENDING),('score', pymongo.DESCENDING)])
-        
-        display_articles=[]
+        articles = db.db.articles.find().sort(
+            [('date', pymongo.DESCENDING), ('score', pymongo.DESCENDING)])
+
+        display_articles = []
         for i in range(10):
-            
+
             display_articles.append({
                 'title': articles[i]['title'],
                 'description': articles[i]['description'],
                 'date': articles[i]['date'],
                 'content': articles[i]['content'],
                 'url': articles[i]['url'],
-                
-
             })
-        response['data']=display_articles
+        response['data'] = display_articles
         return (response)
-    except :
-        response['error']=True
+    except:
+        response['error'] = True
         return jsonify(response)
-        
+
+
 @app.route("/reduce_score")
 def reduceScore():
-    articles=db.db.articles.find().sort([('date',pymongo.DESCENDING),('score', pymongo.DESCENDING)])
+    articles = db.db.articles.find().sort(
+        [('date', pymongo.DESCENDING), ('score', pymongo.DESCENDING)])
     for i in range(10):
-        articles[i]['score']=-1
-        db.db.articles.find_one_and_update({'title':articles[i]['title']},{ '$set':{'score':-1}})
+        articles[i]['score'] = -1
+        db.db.articles.find_one_and_update(
+            {'title': articles[i]['title']}, {'$set': {'score': -1}})
     return "score_reduced"
+
 
 @app.route("/save_bookmark", methods=['POST'])
 def bookmark():
     try:
-        title=request.json['title']
+        title = request.json['title']
 
-            # save this article if the title doesn't already exist in the database
-        bookmark_article=db.db.articles.find_one({'title':title})
-        db.db.articles.delete_one({'title':title})
+        # save this article if the title doesn't already exist in the database
+        bookmark_article = db.db.articles.find_one({'title': title})
+        db.db.articles.delete_one({'title': title})
         db.db.bookmarks.insert_one(bookmark_article)
-    
+
         return "bookmarked"
     except:
         return "no such title"
+
+
 if __name__ == '__main__':
     app.run()
